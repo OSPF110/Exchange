12492357d4e87949f8ad6a2a9cac7ac733f3f20a
EAlderson51 <30420631+EAlderson51@users.noreply.github.com>
2021-05-12 11:13:42 +0100

    Update Maintenance Mode.ps1

--- Maintenance Mode.ps1
+++ Maintenance Mode.ps1
@@ -1,4 +1,6 @@
 ##cmds to enter maintenance mode in exchange for CU update
 C:\>Set-ServerComponentState EX2016SRV1 –Component ServerWideOffline –State InActive –Requester Maintenance
+##Once server is in maintenance mode, mount the ISO then run the following to start the install from a admin cmd prompt
+X:\>setup /m:upgrade /IAcceptExchangeServerLicenseTerms
 ##Once CU update is complete run the following to exit maintenance mode
 C:\>Set-ServerComponentState EX2016SRV1 –Component ServerWideOffline –State Active –Requester Maintenance
