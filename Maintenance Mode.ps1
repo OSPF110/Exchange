30293160ab5c0c6c4f871df54234d4f044e2a914
EAlderson51 <30420631+EAlderson51@users.noreply.github.com>
2022-10-01 18:29:17 +0100

    Update Maintenance Mode.ps1

--- Maintenance Mode.ps1
+++ Maintenance Mode.ps1
@@ -1,7 +1,7 @@
 ##cmds to enter maintenance mode in exchange for CU update
 C:\>Set-ServerComponentState EX2016SRV1 –Component ServerWideOffline –State InActive –Requester Maintenance
 ##Once server is in maintenance mode, mount the ISO then run the following to start the install from a admin cmd prompt
-##Change operations masters to DRVMDC01 via AD-U&C
+##Change operations masters to DRVMDC01 via AD Domain schema snap-in
 X:\>setup /m:upgrade /IAcceptExchangeServerLicenseTerms_DiagnosticDataOFF
 ##Once CU update is complete run the following to exit maintenance mode
 C:\>Set-ServerComponentState EX2016SRV1 –Component ServerWideOffline –State Active –Requester Maintenance
